d41daa3c2761dc6be3a1c8910f3e6e7a068c0070
Yu <yu.xia@bain.com>
2022-12-07 15:48:04 -0800

    check

--- tests/test_check.py
+++ tests/test_check.py
@@ -1,29 +1,17 @@
 import os
-import pandas as pd
 from hydra import initialize, compose
 
-def test():
-    with initialize(version_base=None, config_path="../cfg"):
-        cfg = compose(config_name="env1_cfg")
-    # check raw data
-    raw_data = cfg.paths.dunnhumby_data_path
-    trx = pd.read_csv(raw_data + 'transaction_data.csv')
-    print(trx.columns)
-    assert cfg.colnames.customer_key in trx.columns
-
-def test_preprocess_completed():
+def test_unzip_raw_data():
     # verify if the output files are present in the desired path
     with initialize(version_base=None, config_path="../cfg"):
         cfg = compose(config_name="env1_cfg")
-    # run_preprocess(cfg)
-    processed_data = cfg.paths.processed_data
-    files = os.listdir(processed_data)
+
+    raw_data_path = cfg.paths.dunnhumby_data_path
+    files = os.listdir(raw_data_path)
     # check file presence
-    assert 'customer_filtered.csv' in files
-    assert 'product_filtered.csv' in files
-    assert 'trx_filtered.csv' in files
+    assert "transaction_data.csv" in files
+    assert "hh_demographic.csv" in files
+    assert "product.csv" in files
 
-
-if __name__ == '__main__':
-    test()
-    test_preprocess_completed()
+if __name__ == "__main__":
+    test_unzip_raw_data()
